0699b1feb7cc712d7508dd4676c7d17cc9f8c0a8
Vince Knight <vince@vknight.org>
2026-05-19 22:30:58 +0100

    Add an Error if an incorrect category is used. (#3)

--- src/scholia/__init__.py
+++ src/scholia/__init__.py
@@ -1,5 +1,6 @@
 """Scholia: a marking aid library."""
 
+from scholia.exceptions import CategoryNotInSchemeError
 from scholia.feedback import generate_all_feedback, generate_student_feedback
 from scholia.marks import compute_question_marks, compute_total_marks
 from scholia.scheme import Category, Question, Scheme
@@ -8,6 +9,7 @@ from scholia.students import Student, Students
 
 __all__ = [
     "Category",
+    "CategoryNotInSchemeError",
     "Question",
     "Scheme",
     "Student",
